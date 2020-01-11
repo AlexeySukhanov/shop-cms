a37230988b26a4f6a0b8e5cbf83e860f34275908
AlexeySukhanov <webdevforbiz@gmail.com>
2020-01-11 08:43:41 +0300

    ADD IndexController

---
+++ controllers/IndexController.php
@@ -0,0 +1,6 @@
+<?php
+
+// Тестовый экшн контроллера IndexController
+function testAction() {
+    echo 'IndexController.php > testAction <br />';
+}
--- www/index.php
+++ www/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'it works';
+// Определяем какой контроллер будет использоваться
+$controllerName = isset( $_GET['controller'] ) ? ucfirst( $_GET['controller'] ) : 'Index';
+echo 'Подключаемый php файл (Контроллер): ' . $controllerName . '<br />';
+
+// Определяем какой экшн будет использоваться
+$actionName = isset( $_GET['action'] ) ? $_GET['action'] : 'index';
+echo 'Функция формирующая страницу (Экшн): ' . $actionName . '<br />';
+
+// Подключаем файл с указанным контроллером
+include_once '../controllers/' . $controllerName . 'Controller.php';
+
+testAction();
