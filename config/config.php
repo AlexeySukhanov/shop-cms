71f8e45bfd4fe67a6f2f8fd5ac6713623950eabc
AlexeySukhanov <webdevforbiz@gmail.com>
2020-01-23 14:53:41 +0300

    Smarty init

--- config/config.php
+++ config/config.php
@@ -11,3 +11,28 @@ error_reporting(E_ALL);
 define( 'CONTROLLER_PATH_PREFIX', '../controllers/' );
 define( 'CONTROLLER_PATH_POSTFIX', 'Controller.php' );
 //<
+
+// Используемый шаблон
+$template = 'default';
+
+// Пути к файлам шаблонов
+define('TEMPLATE_PREFIX', "../views/{$template}/");
+define('TEMPLATE_POSTFIX', '.tpl');
+
+// Путь к файлам шаблонов в веб-пространстве
+define('TEMPLATE_WEB_PATH', "/templates/{$template}/");
+
+//> Инициализация шаблонизатора Smarty и его настройка
+require('../library/Smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TEMPLATE_PREFIX);
+$smarty->setCompileDir('../tmp/smarty/templates_c/');
+$smarty->setCacheDir('../tmp/smarty/cache/');
+$smarty->setConfigDir('../library/Smarty/configs/');
+
+$smarty->assign('templateWebPath', 'TEMPLATE_WEB_PATH');
+//<
+
+
+
